6dfc19ad626776ba4cb358fb6ba7e8e5ef88fcfb
Liam Kikuchi <liamshinx@gmail.com>
2018-09-22 16:01:42 -0700

    Working ladders + Jump animation

--- Dimension Delta/Assets/Scripts/LadderCode.cs
+++ Dimension Delta/Assets/Scripts/LadderCode.cs
@@ -4,34 +4,37 @@ using UnityEngine;
 
 public class LadderCode : MonoBehaviour {
 
-    private Transform lT;
     public Rigidbody2D player;
+    public int climb;
     GridLayout gridLayout;
     Vector3Int cellPosition;
     Vector3 place;
 
     void Start()
     {
-        lT = GetComponent<Transform>();
         gridLayout = transform.parent.GetComponentInParent<GridLayout>();
     }
 
     private void OnTriggerStay2D(Collider2D other)
     {
-        if(Input.GetKeyDown("up") == true)
+        //Going Up!
+        if (Input.GetKey("up") == true || Input.GetAxis("Vertical") > 0)
         {
-            //cellPosition = gridLayout.WorldToCell(new Vector3((int)(other.transform.position.x + .05), other.transform.position.y, other.transform.position.z));
-            //place = gridLayout.CellToWorld(cellPosition);
-            //other.transform.SetPositionAndRotation(new Vector2(place.x + (float).5, other.transform.position.y), new Quaternion());
-            //player.constraints = RigidbodyConstraints2D.FreezePositionX;
+            cellPosition = gridLayout.WorldToCell(new Vector3((int)(other.transform.position.x + .05), other.transform.position.y, other.transform.position.z));
+            place = gridLayout.CellToWorld(cellPosition);
+            other.transform.SetPositionAndRotation(new Vector2(place.x + (float).5, other.transform.position.y), new Quaternion());
+            player.constraints = RigidbodyConstraints2D.FreezeRotation;
+            player.AddForce(new Vector2(0, 3 * climb));
+            player.velocity.Set(0, player.velocity.y);
         }
         else
         {
-            //player.constraints = RigidbodyConstraints2D.None;
+           
         }
+
     }
     private void OnTriggerExit2D(Collider2D collision)
     {
-        //player.constraints = RigidbodyConstraints2D.None;
+        player.constraints = RigidbodyConstraints2D.FreezeRotation;
     }
 }
--- Dimension Delta/Assets/Scripts/PlayerController.cs
+++ Dimension Delta/Assets/Scripts/PlayerController.cs
@@ -7,6 +7,7 @@ using UnityEngine.UI;
 public class PlayerController : MonoBehaviour {
 
     public float force = 15;
+    public float jump = 50;
     public Collider2D ladderC2d;
     public Transform ladderT;
     Rigidbody2D rb;
@@ -17,17 +18,18 @@ public class PlayerController : MonoBehaviour {
         animator = GetComponent<Animator>();
         rb = GetComponent<Rigidbody2D>();
         sr = GetComponent<SpriteRenderer>();
+        //rb.constraints = RigidbodyConstraints2D.None;
     }
 	
 	void FixedUpdate () {
         rb.AddForce(new Vector2(Input.GetAxis("Horizontal") * force, (float).2)); //get input from keyboard/controller
-        //Good speed from 15 force, coming to a quick stop is thanks to linear drag on rigid body
+        //Good speed from 23 force, coming to a quick stop is thanks to the 2 linear drag on rigid body and 3 grav
 
-        if (rb.velocity.x > 0) //Does the player face right?
+        if (rb.velocity.x > 0 && rb.constraints != RigidbodyConstraints2D.FreezePositionX) //Does the player face right?
         {
             sr.flipX = false; 
         }
-        else if(rb.velocity.x < 0) //Or are they facing left?
+        else if(rb.velocity.x < 0 && rb.constraints != RigidbodyConstraints2D.FreezePositionX) //Or are they facing left?
         {
             sr.flipX = true;
         }
@@ -38,6 +40,16 @@ public class PlayerController : MonoBehaviour {
         else // No input
         {
             animator.SetInteger("speed" , 0);//Reroutes loop to idle
+        }
+        if (Input.GetAxis("Jump") > 0 && animator.GetBool("Grounded") == true) //Need to press jump and be on the ground to jump
+        {
+            animator.SetBool("Jumping", true); //Jump squat
+            rb.AddForce(Vector2.up * jump);
+            animator.SetBool("Grounded", false);
+        }
+        if(animator.GetBool("Grounded") == true)
+        {
+            animator.SetBool("Jumping", false);
         }
     }
 
